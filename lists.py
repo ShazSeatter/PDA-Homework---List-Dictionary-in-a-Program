dd1e9144dd21a24a9b092a65ca3961a15d82ead6
Shazreen Seatter <shazseatter12@gmail.com>
2023-01-30 21:03:13 +0000

    created new list, and defined new function

---
+++ lists.py
@@ -0,0 +1,12 @@
+
+book_genres = ["Mystery", "Horror", "Romance", "Fantasy", "Thriller"]
+
+
+def add_genre():
+    book_genres.append("Science Fiction")
+    print(book_genres)
+
+
+add_genre()
+
+
