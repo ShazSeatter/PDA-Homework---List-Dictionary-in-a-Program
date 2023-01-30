f91d6053a6cf6df1570835dce3bb3c28af976237
Shazreen Seatter <shazseatter12@gmail.com>
2023-01-30 21:12:17 +0000

    added list explanation

--- lists.py
+++ lists.py
@@ -10,3 +10,5 @@ def add_genre():
 add_genre()
 
 
+# Lists are a collection of mutable data. Items contained in a list can be any data type. Each item in a list can be accessed by an index. 
+# Lists start at index 0. Lists have access to different methods in which you can then use on a list to do different things with it. For example, add, delete, and sort elements within it. 
